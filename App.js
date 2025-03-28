cd67f8b650ff023baf508ff419008df490972151
hwooch <hwoochhh@naver.com>
2025-03-28 15:25:16 +0900

    AI 대화 화면 스택 추가

--- App.js
+++ App.js
@@ -83,6 +83,7 @@ export default function App() {
         <Stack.Screen name="InquiryForm" component={InquiryFormScreen} options={{ title: '문의하기' }} />
         <Stack.Screen name="InquiryList" component={InquiryListScreen} options={{ title: '문의 내역' }} />
      
+        <Stack.Screen name="VoiceInput" component={VoiceInputScreen} options={{ title: 'AI 대화' }} />
       </Stack.Navigator>
     </NavigationContainer>
     </FontSizeProvider>
