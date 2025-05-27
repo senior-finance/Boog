e88eaa6d133e6e1eacd4d7816c65ec267b1cf073
hwooch <hwoochhh@naver.com>
2025-05-27 18:37:48 +0900

    로딩 배경 변경

--- src/screens/Info/WebScreen.js
+++ src/screens/Info/WebScreen.js
@@ -2,25 +2,35 @@ import React, { useState } from 'react';
 import { View, Text, StyleSheet, ActivityIndicator } from 'react-native';
 import { WebView } from 'react-native-webview';
 import CustomText from '../../components/CustomText';
+import LottieView from 'lottie-react-native';
+import LinearGradient from 'react-native-linear-gradient';
+import loadingJson from '../../assets/loadingg.json';
 
 const WebScreen = ({ route }) => {
   const { url } = route.params;
-  const [loading, setLoading] = useState(true); // 로딩 상태
+  const [loading, setLoading] = useState(true);
 
   return (
     <View style={{ flex: 1 }}>
       {loading && (
-        <View style={styles.loadingContainer}>
-          <ActivityIndicator size="large" color="#4B7BE5" />
-          <CustomText style={styles.loadingText}>로딩 중입니다...</CustomText>
-        </View>
+        <LinearGradient
+          colors={['rgba(140, 182, 222, 0.69)', '#e0f0ff']}
+          style={styles.loadingContainer}
+        >
+          <LottieView
+            source={loadingJson}
+            autoPlay
+            loop
+            style={{ width: 160, height: 160 }}
+          />
+        </LinearGradient>
       )}
       <WebView
         source={{ uri: url }}
-        onLoadStart={() => setLoading(true)} // 로딩 시작
-        onLoadEnd={() => setLoading(false)} // 로딩 끝
-        javaScriptEnabled={true}
-        domStorageEnabled={true}
+        onLoadStart={() => setLoading(true)}
+        onLoadEnd={() => setLoading(false)}
+        javaScriptEnabled
+        domStorageEnabled
       />
     </View>
   );
@@ -28,19 +38,10 @@ const WebScreen = ({ route }) => {
 
 const styles = StyleSheet.create({
   loadingContainer: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    backgroundColor: '#fff',
+    ...StyleSheet.absoluteFillObject,
     justifyContent: 'center',
     alignItems: 'center',
-    zIndex: 1, // WebView 위에 표시
-  },
-  loadingText: {
-    marginTop: 15,
-        color: '#333',
+    zIndex: 1,
   },
 });
 
