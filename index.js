e853e2c065491a1b9c51b33229659a9821e2fa50
hwooch <hwoochhh@naver.com>
2025-05-29 17:22:38 +0900

    에러 화면 수정

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ LogBox.ignoreLogs([
     "`new NativeEventEmitter()` was called with a non-null argument without the required `addListener` method.",
     "`new NativeEventEmitter()` was called with a non-null argument without the required `removeListeners` method.",
     'ReactImageView: Image source "null" doesn\'t exist',
+    'ViewPropTypes will be removed from React Native',
+    'new NativeEventEmitter',
 ]);
 import { AppRegistry } from 'react-native';
 import App from './App';
